4cfb43d9511dce164cdc9fb8ff09557c73d73000
ZodyT <Zoraida1988@yahoo.com>
2020-11-13 19:44:04 -0600

    Beginning

---
+++ ZTextAdventure/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ZTextAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello Welcome to my animal generator");
+
+            Console.WriteLine("What is your favorite animal?");
+            string animal = Console.ReadLine();
+
+            Console.WriteLine("Pick a Color: Red or Blue?");
+            string Color = Console.ReadLine();
+            Color = Color.ToLower();
+
+            Console.WriteLine("Pick a random number.");
+            string Legs = Console.ReadLine();
+            Convert.ToInt32(Legs);
+
+
+
+        }
+    }
+}
