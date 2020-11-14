00f9804acf3c941ce5ca7a09e667ed5190fcd7a7
ZodyT <Zoraida1988@yahoo.com>
2020-11-14 02:22:56 -0600

    Color Loop

--- ZTextAdventure/Program.cs
+++ ZTextAdventure/Program.cs
@@ -20,72 +20,89 @@ namespace ZTextAdventure
 
                 Console.WriteLine();
 
-                Console.Write("Pick a Color: ");
-                Console.ForegroundColor  = ConsoleColor.Yellow;
-                Console.Write("Yellow");
-                Console.ResetColor();
-                Console.Write(" or ");
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine("Blue");
-                Console.ResetColor();
-
-                string Color = Console.ReadLine();
-                Color = Color.ToLower();
+                Console.WriteLine("Pick a random number");
+                string Legs = Console.ReadLine();
+                Convert.ToInt32(Legs);
 
                 Console.WriteLine();
 
-                    Console.WriteLine("Pick a random number");
-                    string Legs = Console.ReadLine();
-                    Convert.ToInt32(Legs);
+                bool Error = true;
+                while (Error)
+                {
+                    Console.Write("Pick a Color: ");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write("Yellow");
+                    Console.ResetColor();
+                    Console.Write(" or ");
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Blue");
+                    Console.ResetColor();
 
-                Console.WriteLine();
+                    string Color = Console.ReadLine().ToLower();
 
-                Console.WriteLine("To Generate your animal press Enter");
+
+                    Console.WriteLine();
+                    Console.WriteLine("To Generate your animal press Enter");
                     Console.ReadLine();
 
                     if (Color == "yellow")
                     {
-                    Console.Write("Your animal is a " + Animal + "\nit is ");
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.Write("yellow");
-                    Console.ResetColor();
-                    Console.WriteLine(" with 1 eye \nand it has " + Legs + " legs \n");
-                    }
-                    else
-                    {
-                    Console.Write("Your animal is a " + Animal + "\nit is ");
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.Write("blue");
-                    Console.ResetColor();
-                    Console.WriteLine(" with 5 eyes \nand it has " + Legs + " legs \n");
+                        Console.WriteLine();
+                        Console.Write("Your animal is a " + Animal + "\nit is ");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write("yellow");
+                        Console.ResetColor();
+                        Console.WriteLine(" with 1 eye \nand it has " + Legs + " legs \n");
                     }
 
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Would you like to generate another animal?");
-                    Console.ResetColor();
-                    string Response = Console.ReadLine();
-                    Response = Response.ToLower();
-                    
+                    else if (Color == "blue")
+                    {
+                        Console.WriteLine();
+                        Console.Write("Your animal is a " + Animal + "\nit is ");
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write("blue");
+                        Console.ResetColor();
+                        Console.WriteLine(" with 5 eyes \nand it has " + Legs + " legs \n");
+                    }
+
+                    else if (Color != "yellow" && Color != "blue")
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Please try again");
+                        Console.WriteLine();
+                        Error = true;
+                    }
+                    else //if (Color == "yellow" || Color == "blue")
+                    {
+                        //Console.WriteLine();
+                        //Error = false;
+                        continue;
+                    }
+                }
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Would you like to generate another animal?");
+                Console.ResetColor();
+                string Response = Console.ReadLine().ToLower();
 
                 if (Response == "yes")
-                    {
-                        Console.WriteLine();
-                        Answer = true;
-                    }
+                {
+                    Console.WriteLine();
+                    Answer = true;
+                }
 
-                    else
-                    {
-                        Console.WriteLine();
-                        Answer = false;
-                        Console.ForegroundColor = ConsoleColor.Cyan;
-                        Console.WriteLine("Thank you for using my animal generator!");
-                        Console.ResetColor();
+                else
+                {
+                    Console.WriteLine();
+                    Answer = false;
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("Thank you for using my animal generator!");
+                    Console.ResetColor();
 
                     Console.WriteLine();
                     Console.WriteLine();
                     Console.WriteLine();
                 }
-                
+
             }
         }
     }
